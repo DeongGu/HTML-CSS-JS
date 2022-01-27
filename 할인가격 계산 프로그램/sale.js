838c72a6db9d7fde8c3e0c9fbf3dfd8eff9a9faf
Seongwoo Shin <asdfgg77@naver.com>
2022-01-27 17:08:18 +0900

    템플릿 문자열

--- 할인가격 계산 프로그램/sale.js
+++ 할인가격 계산 프로그램/sale.js
@@ -4,5 +4,5 @@ function showPrice() {
     var result = price * (rate/100);
     var lastResult = price - result;
 
-    document.querySelector('#showResult').innerHTML = "상품의 원래가격은 " + price + "원이고, 할인율은 " + rate + "% 입니다. " + result +"원을 절약한 "+ lastResult +"원에 살 수 있습니다."
+    document.querySelector('#showResult').innerHTML = `상품의 원래가격은 ${price}원이고, 할인율은 ${rate}% 입니다. ${result}원을 절약한 ${lastResult}원에 살 수 있습니다.`
 }
