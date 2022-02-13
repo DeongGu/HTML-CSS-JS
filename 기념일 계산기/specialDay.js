b9eaaaa3e29d540e4e281946e426459f76a65743
Seongwoo Shin <asdfgg77@naver.com>
2022-02-13 21:33:41 +0900

    null값일 경우 리로드

--- 기념일 계산기/specialDay.js
+++ 기념일 계산기/specialDay.js
@@ -1,9 +1,9 @@
-var test = prompt("그녀와 사귀기 시작한 날짜는?", "2022-02-13");
-if(test == null) {
+let start = prompt("그녀와 사귀기 시작한 날짜는?", "2022-02-13");
+if(start == null) {
     window.location.reload();
 }
 
-    var firstMeetDay = new Date(test);
-    var currentDay = new Date();
-    var result = Math.round((currentDay.getTime() - firstMeetDay.getTime())/(1000*60*60*24)) ;
+    let firstMeetDay = new Date(start);
+    let currentDay = new Date();
+    let result = Math.round((currentDay.getTime() - firstMeetDay.getTime())/(1000*60*60*24)) ;
     document.querySelector('#dateDay').innerHTML = result + "일";
