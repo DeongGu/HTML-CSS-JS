18a5b9270ba920530803f71eeceec6c44603a129
Seongwoo Shin <asdfgg77@naver.com>
2022-03-10 00:13:02 +0900

    리스트 만들기

---
+++ 바닐라js연습/js/todo.js
@@ -0,0 +1,45 @@
+const toDoForm = document.querySelector("#todoForm");
+const toDoList = document.querySelector("#todoList");
+const toDoInput = document.querySelector("#todoForm input")
+
+const TODOS_KEY ="todos";
+
+const toDos = [];
+
+function saveToDos(){
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+function deleteToDo(event){
+    const li = event.target.parentElement;
+    li.remove();
+}
+function paintToDo(newTodo) {
+    const li = document.createElement("li")
+    const span = document.createElement("span");
+    span.innerText = newTodo;
+    const button = document.createElement("button");
+    button.innerText = "X";
+    button.addEventListener("click",  deleteToDo);
+    li.appendChild(span);
+    li.appendChild(button);
+    toDoList.appendChild(li);
+}
+function handleToDoSubmit(event) {
+    event.preventDefault();
+    const newTodo = toDoInput.value;
+    toDoInput.value ="";
+    toDos.push(newTodo);
+    paintToDo(newTodo);
+    saveToDos();
+}
+
+toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if (savedToDos !== null) {
+    const parsedToDos = JSON.parse(savedToDos);
+    parsedToDos.ewwarray.forEach(element => {
+        
+    });
+}
