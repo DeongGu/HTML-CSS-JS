285a68b24b1822acca209d3d1e30739e4c21aa3b
Seongwoo Shin <asdfgg77@naver.com>
2022-03-09 17:31:42 +0900

    무작위 요소 연습

---
+++ 바닐라js연습/js/background.js
@@ -0,0 +1,9 @@
+const images = ["백수.jpg","살찐 백수.jpg", "오리지널 백수.png"];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = document.createElement("img");
+
+bgImage.src = `/img/${chosenImage}`;
+
+document.body.appendChild(bgImage);
