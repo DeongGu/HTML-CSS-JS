c898c5a399beac2994e025db92468d47d6c237a9
Seong Woo <95382684+DeongGu@users.noreply.github.com>
2022-03-26 16:22:27 +0900

    this (메서드에서 활용)

--- this.js
+++ this.js
@@ -8,3 +8,16 @@ var kim = {
 }
 // console.log("kim.sum(kim.first, kim.second)", kim.sum(kim.first, kim.second));
 console.log("kim.sum(kim.first, kim.second)", kim.sum());
+
+
+const shin = {
+    name: 'shin',
+    IQ: 140,
+    score: 100,
+    penalty: 20,
+    sum: function(){
+       return this.score-this.penalty;
+    }
+}
+
+console.log(shin.sum());
