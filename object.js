7d0ddbd0d0f91f61e3428cccfe35d759f32da4a2
Seong Woo <95382684+DeongGu@users.noreply.github.com>
2022-03-26 15:23:35 +0900

    객체 생성,수정,삭제

--- object.js
+++ object.js
@@ -11,3 +11,41 @@ console.log("memberObject.designer", memberObject.designer);
 console.log("memberObject['designer']", memberObject['designer']);
 delete memberObject.manager
 console.log('after delete memberObject.manager', memberObject.manager);
+
+const brandArray = ['nike', 'adidas','samsung'];
+console.log('brandArray[2]', brandArray[2]);
+
+
+//객체 생성
+const brandObject = {
+    IT: 'samsung',
+    shoes: 'nike',
+    trainning: 'adidas',
+    'mouse': 'logetech',
+    badminton: 'lonex'
+}
+
+// 프로퍼티(키) 값 수정, 추가
+brandObject.badminton = 'YONEX'; 
+console.log(brandObject.badminton);
+
+//배열과는 다른 객체에서 값을 불러오는 방법(.프로퍼티, ['프로퍼티'])
+console.log('brandObject.IT', brandObject.IT);
+console.log("brandObject['shoes']", brandObject['shoes']); // console.log("brandObject[shoes]", brandObject[shoes]);//ReferenceError: shoes is not defined
+
+console.log(brandObject['trainning']);
+console.log(brandObject);
+
+// {mouse: } ,{'mouse': } 비교
+console.log('brandObject.mouse', brandObject.mouse); // console.log("brandObject['mouse']",brandObject[mouse]); ReferenceError: mouse is not defined
+console.log("brandObject['mouse']",brandObject['mouse']);
+
+//객체 프로퍼티 삭제
+delete brandObject.mouse;
+console.log(brandObject);
+// {
+//     IT: 'samsung',
+//     shoes: 'nike',
+//     trainning: 'adidas',
+//     badminton: 'YONEX'
+// }
