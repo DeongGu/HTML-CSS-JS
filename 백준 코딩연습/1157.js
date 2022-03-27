b42c26306ef8d2954d90962d574f70aa8877a562
Seong Woo <95382684+DeongGu@users.noreply.github.com>
2022-03-28 00:02:13 +0900

    문자열 문제

--- 백준 코딩연습/1157.js
+++ 백준 코딩연습/1157.js
@@ -1,31 +1,63 @@
 const fs = require('fs');
-const input = fs.readFileSync('test.txt').toString().trim();
+const input = fs.readFileSync('/dev/stdin').toString().trim();
 
 
 solution();
 
 function solution() {
-    let a = input.toUpperCase();
-    let b = Array(a.length).fill(0);
+    let str = {};
+    for(let i =0; i<input.length; i++){
+        if(str[input[i].toUpperCase()] === undefined ){
+            str[input[i].toUpperCase()] = 1;
+        }else{
+            str[input[i].toUpperCase()]++
+        };
+    }
+    
+    let maxCount = 0;
+    let maxStr = "";
+    let isOneMore = false;
 
-    for(let i = 0; i<b.length; i++){
-        for(let j = i+1; j<b.length; j++){
-            if(a[i] === a[j]){
-            b[i]++
-            }
+    for(const a in str){
+        if(str[a] > maxCount){
+            maxCount = str[a];
+            maxStr = a;
+            isOneMore = false;
+        }else if(str[a] === maxCount){
+            isOneMore = true;
         }
     }
 
-let count = 0;
-for(let i = 0; i<b.length; i++){
-    if(Math.max(...b) === b[i]){
-        count++
+    if(isOneMore){
+        console.log('?');
+    }else{
+        console.log(maxStr);
     }
+}
+
+
+// function solution() {
+//     let a = input.toUpperCase();
+//     let b = Array(a.length).fill(0);
+
+//     for(let i = 0; i<b.length; i++){
+//         for(let j = i+1; j<b.length; j++){
+//             if(a[i] === a[j]){
+//             b[i]++
+//             }
+//         }
+//     }
+
+// let count = 0;
+// for(let i = 0; i<b.length; i++){
+//     if(Math.max(...b) === b[i]){
+//         count++
+//     }
     
-}
-if(count > 1){
-    console.log("?");
-}else{
-    console.log(a[Math.max(...b)]);
-}
-}
+// }
+// if(count > 1){
+//     console.log("?");
+// }else{
+//     console.log(a[Math.max(...b)]);
+// }
+// }
