3d65a4627357da46f92d5b898d7e462777f258c7
Seong Woo <95382684+DeongGu@users.noreply.github.com>
2022-04-02 22:21:19 +0900

    문제 풀이 연습

--- 백준 코딩연습/2775.js
+++ 백준 코딩연습/2775.js
@@ -1,3 +1,10 @@
 const fs = require('fs');
 const input = fs.readFileSync('test.txt').toString().trim().split('\n');
 
+const N = +input[0];
+const potion = {};
+for(let i = 0; i<N; i++){
+    potion[i+1]=+input[1].split(' ')[i];
+}
+const P = input.slice(2);
+console.log(P);
