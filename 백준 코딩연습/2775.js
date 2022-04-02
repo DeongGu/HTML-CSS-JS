d241808c3517297396750210d69e31a4734d9fe3
Seong Woo <95382684+DeongGu@users.noreply.github.com>
2022-04-03 00:02:39 +0900

    입력 내용 표현

--- 백준 코딩연습/2775.js
+++ 백준 코딩연습/2775.js
@@ -1,10 +1,17 @@
 const fs = require('fs');
 const input = fs.readFileSync('test.txt').toString().trim().split('\n');
+const T = +input[0];
+const testCase =[];
 
-const N = +input[0];
-const potion = {};
-for(let i = 0; i<N; i++){
-    potion[i+1]=+input[1].split(' ')[i];
+//[2,1,3,2,3]
+for(let i = 1; i < input.length; i+=2){
+    testCase.push(input.slice(i,i+2));
 }
-const P = input.slice(2);
-console.log(P);
+
+console.log(testCase);
+
+//1,3 0층 3호까지 합  1+2+3=6             1 1 1
+//2,3 1층 3호까지 합 1+3+6=10             2 3 4
+//3,3 2층 3호까지 합 1+4+9=14             3 5 7
+
+//4,5 3층 5호까지 합 1+5+12+16=34
