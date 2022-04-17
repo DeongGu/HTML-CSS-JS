7018628d03ee07180fda5cc60a102eaeb885bcb9
Seong Woo <95382684+DeongGu@users.noreply.github.com>
2022-04-17 16:07:59 +0900

    타이머 종료 구현

--- 타이머 프로그램/js/timer.js
+++ 타이머 프로그램/js/timer.js
@@ -5,26 +5,41 @@ const resetBtn = document.querySelector(".reset-btn");
 const remainResult = document.querySelector("#display");
 
 
-
+var trigger;
 function start() {
-    let min = Number(startMin.value);
-    let sec = Number(startSec.value);
+    let min = startMin.value;
+    let sec = startSec.value;
+  if(min === ""){
+      min = 0;
+  }
+  if(sec === ""){
+      sec = 0;
+  }
+  trigger = setInterval(timer, 1000);
 
-    setInterval(timer,1000);
-    function timer() {
-        if(sec === 0){
-            min-= 1;
-            sec = 60;
-            }
-        }
+  function timer() {
+    if(sec !== 0){
+        remainResult.innerHTML=`${min}분 ${sec}초`; 
         sec-= 1;
-        remainResult.innerHTML=`${min} ${sec}`;
-
+    } else {
+        if(min !==0){
+            min -= 1;
+            sec = 59;
+        } else {
+            clearInterval(trigger);
+            remainResult.innerHTML = "타이머 종료";
+        }
     }
-    
+            
+  }
+        
 }
+
 function reset(){
-    remainResult.innerHTML="";
+    startMin.value = "";
+    startSec.value = "";
+    remainResult.innerHTML = "";
+    clearInterval(trigger);
 }
 
 startBtn.addEventListener('click', start);
