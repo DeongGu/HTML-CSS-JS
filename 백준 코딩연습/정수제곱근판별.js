26692f8df9000af84c26e4eeb25723ed675a54ad
Seong Woo <95382684+DeongGu@users.noreply.github.com>
2022-04-24 23:52:27 +0900

    Math.sqrt(), Math.pow(), Number.isInteger()

--- 백준 코딩연습/정수제곱근판별.js
+++ 백준 코딩연습/정수제곱근판별.js
@@ -2,5 +2,5 @@ const n = 121;
 // const n = 3;
 
 function solution(n) {
-    return Number.isInteger(Math.sqrt(n)) !== undefined ? (Math.sqrt(n)+1)**2 : -1;
+    return Number.isInteger(Math.sqrt(n)) ? (Math.sqrt(n)+1)**2 : -1;
 }
