629054f7601b9297196abef6f5213814e9c9ea1f
Seongwoo Shin <asdfgg77@naver.com>
2022-07-11 19:59:16 +0900

    DOM

---
+++ JavaScript 연습/elice/03/dom/dom.js
@@ -0,0 +1,19 @@
+const chicken = document.getElementById("best");
+
+const best = document.getElementsByClassName("best");
+console.log(chicken);
+console.log(best);
+
+// const chicken = document.querySelector("p");
+// const bestchicken = document.querySelector(".best");
+
+// const para = document.querySelector("body > h3");
+
+// const checkall = document.querySelectorAll("p");
+
+//맨 처음의 tag를 인식해서 가져온다.
+
+// console.log(chicken);
+// console.log(bestchicken);
+// console.log(para);
+// console.log(checkall);
